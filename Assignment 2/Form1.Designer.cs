c62609be9de8f1b7e79fca86030b001e7f01875e
Steven Macpherson <rc0039062@student.riversidecollege.ac.uk>
2020-01-13 15:37:57 +0000

    Add project files.

---
+++ Assignment 2/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Assignment_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox105 = new System.Windows.Forms.TextBox();
+            this.textBox112 = new System.Windows.Forms.TextBox();
+            this.textBox110 = new System.Windows.Forms.TextBox();
+            this.textBox107 = new System.Windows.Forms.TextBox();
+            this.textBox101 = new System.Windows.Forms.TextBox();
+            this.textBox103 = new System.Windows.Forms.TextBox();
+            this.textBox114 = new System.Windows.Forms.TextBox();
+            this.textBox106 = new System.Windows.Forms.TextBox();
+            this.textBox102 = new System.Windows.Forms.TextBox();
+            this.textBox104 = new System.Windows.Forms.TextBox();
+            this.textBox113 = new System.Windows.Forms.TextBox();
+            this.textBox111 = new System.Windows.Forms.TextBox();
+            this.textBox108 = new System.Windows.Forms.TextBox();
+            this.textBox109 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(97, 44);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(97, 435);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 1;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(653, 435);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 2;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(653, 44);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 3;
+            this.pictureBox8.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(112, 15);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(666, 15);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(112, 406);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(666, 406);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // textBox105
+            // 
+            this.textBox105.Location = new System.Drawing.Point(702, 100);
+            this.textBox105.Name = "textBox105";
+            this.textBox105.Size = new System.Drawing.Size(100, 20);
+            this.textBox105.TabIndex = 8;
+            this.textBox105.Text = "105";
+            // 
+            // textBox112
+            // 
+            this.textBox112.Location = new System.Drawing.Point(702, 491);
+            this.textBox112.Name = "textBox112";
+            this.textBox112.Size = new System.Drawing.Size(100, 20);
+            this.textBox112.TabIndex = 9;
+            this.textBox112.Text = "112";
+            // 
+            // textBox110
+            // 
+            this.textBox110.Location = new System.Drawing.Point(157, 517);
+            this.textBox110.Name = "textBox110";
+            this.textBox110.Size = new System.Drawing.Size(100, 20);
+            this.textBox110.TabIndex = 10;
+            this.textBox110.Text = "110";
+            // 
+            // textBox107
+            // 
+            this.textBox107.Location = new System.Drawing.Point(51, 491);
+            this.textBox107.Name = "textBox107";
+            this.textBox107.Size = new System.Drawing.Size(100, 20);
+            this.textBox107.TabIndex = 11;
+            this.textBox107.Text = "107";
+            // 
+            // textBox101
+            // 
+            this.textBox101.Location = new System.Drawing.Point(51, 100);
+            this.textBox101.Name = "textBox101";
+            this.textBox101.Size = new System.Drawing.Size(100, 20);
+            this.textBox101.TabIndex = 12;
+            this.textBox101.Text = "101";
+            // 
+            // textBox103
+            // 
+            this.textBox103.Location = new System.Drawing.Point(97, 126);
+            this.textBox103.Name = "textBox103";
+            this.textBox103.Size = new System.Drawing.Size(100, 20);
+            this.textBox103.TabIndex = 13;
+            this.textBox103.Text = "103";
+            // 
+            // textBox114
+            // 
+            this.textBox114.Location = new System.Drawing.Point(702, 517);
+            this.textBox114.Name = "textBox114";
+            this.textBox114.Size = new System.Drawing.Size(100, 20);
+            this.textBox114.TabIndex = 14;
+            this.textBox114.Text = "114";
+            // 
+            // textBox106
+            // 
+            this.textBox106.Location = new System.Drawing.Point(653, 126);
+            this.textBox106.Name = "textBox106";
+            this.textBox106.Size = new System.Drawing.Size(100, 20);
+            this.textBox106.TabIndex = 15;
+            this.textBox106.Text = "106";
+            // 
+            // textBox102
+            // 
+            this.textBox102.Location = new System.Drawing.Point(157, 100);
+            this.textBox102.Name = "textBox102";
+            this.textBox102.Size = new System.Drawing.Size(100, 20);
+            this.textBox102.TabIndex = 16;
+            this.textBox102.Text = "102";
+            // 
+            // textBox104
+            // 
+            this.textBox104.Location = new System.Drawing.Point(596, 100);
+            this.textBox104.Name = "textBox104";
+            this.textBox104.Size = new System.Drawing.Size(100, 20);
+            this.textBox104.TabIndex = 17;
+            this.textBox104.Text = "104";
+            // 
+            // textBox113
+            // 
+            this.textBox113.Location = new System.Drawing.Point(596, 517);
+            this.textBox113.Name = "textBox113";
+            this.textBox113.Size = new System.Drawing.Size(100, 20);
+            this.textBox113.TabIndex = 18;
+            this.textBox113.Text = "113";
+            // 
+            // textBox111
+            // 
+            this.textBox111.Location = new System.Drawing.Point(596, 491);
+            this.textBox111.Name = "textBox111";
+            this.textBox111.Size = new System.Drawing.Size(100, 20);
+            this.textBox111.TabIndex = 19;
+            this.textBox111.Text = "111";
+            // 
+            // textBox108
+            // 
+            this.textBox108.Location = new System.Drawing.Point(157, 491);
+            this.textBox108.Name = "textBox108";
+            this.textBox108.Size = new System.Drawing.Size(100, 20);
+            this.textBox108.TabIndex = 20;
+            this.textBox108.Text = "108";
+            // 
+            // textBox109
+            // 
+            this.textBox109.Location = new System.Drawing.Point(51, 517);
+            this.textBox109.Name = "textBox109";
+            this.textBox109.Size = new System.Drawing.Size(100, 20);
+            this.textBox109.TabIndex = 21;
+            this.textBox109.Text = "109";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(846, 596);
+            this.Controls.Add(this.textBox109);
+            this.Controls.Add(this.textBox108);
+            this.Controls.Add(this.textBox111);
+            this.Controls.Add(this.textBox113);
+            this.Controls.Add(this.textBox104);
+            this.Controls.Add(this.textBox102);
+            this.Controls.Add(this.textBox106);
+            this.Controls.Add(this.textBox114);
+            this.Controls.Add(this.textBox103);
+            this.Controls.Add(this.textBox101);
+            this.Controls.Add(this.textBox107);
+            this.Controls.Add(this.textBox110);
+            this.Controls.Add(this.textBox112);
+            this.Controls.Add(this.textBox105);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox105;
+        private System.Windows.Forms.TextBox textBox112;
+        private System.Windows.Forms.TextBox textBox110;
+        private System.Windows.Forms.TextBox textBox107;
+        private System.Windows.Forms.TextBox textBox101;
+        private System.Windows.Forms.TextBox textBox103;
+        private System.Windows.Forms.TextBox textBox114;
+        private System.Windows.Forms.TextBox textBox106;
+        private System.Windows.Forms.TextBox textBox102;
+        private System.Windows.Forms.TextBox textBox104;
+        private System.Windows.Forms.TextBox textBox113;
+        private System.Windows.Forms.TextBox textBox111;
+        private System.Windows.Forms.TextBox textBox108;
+        private System.Windows.Forms.TextBox textBox109;
+    }
+}
+
---
+++ Assignment 2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox14_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
